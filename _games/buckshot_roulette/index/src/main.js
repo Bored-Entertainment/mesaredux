3b60e5763c3fabb338787cc5439e0951ef526176
greymesa <58270183+greymesa@users.noreply.github.com>
2025-12-01 23:44:47 -0500

    first effort

--- _games/buckshot_roulette/index/src/main.js
+++ _games/buckshot_roulette/index/src/main.js
@@ -1,47 +1,17 @@
-const originalFetch = window.fetch;
+(function () {
+    let started = false;
 
-function mergeFiles(fileParts) {
-    return new Promise((resolve, reject) => {
-        let buffers = [];
-
-        function fetchPart(index) {
-            if (index >= fileParts.length) {
-                let mergedBlob = new Blob(buffers);
-                let mergedFileUrl = URL.createObjectURL(mergedBlob);
-                resolve(mergedFileUrl);
-                return;
-            }
-            fetch(fileParts[index]).then((response) => {
-                if (!response.ok) throw new Error("Missing part: " + fileParts[index]);
-                return response.arrayBuffer();
-            }).then((data) => {
-                buffers.push(data);
-                fetchPart(index + 1);
-            }).catch(reject);
+    function attemptStart() {
+        if (started) {
+            return;
         }
-        fetchPart(0);
-    });
-}
-
-function getParts(file, start, end) {
-    let parts = [];
-    for (let i = start; i <= end; i++) {
-        parts.push(file + ".part" + i);
+        if (typeof window.godotRunStart === 'function') {
+            started = true;
+            window.godotRunStart();
+            return;
+        }
+        requestAnimationFrame(attemptStart);
     }
-    return parts;
-}
-Promise.all([
-    mergeFiles(getParts("buckshot-roulette.pck", 1, 17)),
-    mergeFiles(getParts("buckshot-roulette.wasm", 1, 3))
-]).then(([pckUrl, wasmUrl]) => {
-    window.fetch = async function (url, ...args) {
-        if (url.endsWith("buckshot-roulette.pck")) {
-            return originalFetch(pckUrl, ...args);
-        } else if (url.endsWith("buckshot-roulette.wasm")) {
-            return originalFetch(wasmUrl, ...args);
-        } else {
-            return originalFetch(url, ...args);
-        }
-    };
-    window.godotRunStart();
-});
+
+    attemptStart();
+}());
